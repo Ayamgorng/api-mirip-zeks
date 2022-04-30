5ebcc78c2bbbbae45d4c6a6c1e6f9ffb270d2611
ZeusFtrOfc <77143853+ZeusFtrOfc@users.noreply.github.com>
2022-04-30 22:24:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-   
+   cors = require('cors'),
+   secure = require('ssl-express-www');
 const PORT = process.env.PORT || 8080 || 5000 || 3000
 var { color } = require('./lib/color.js')
 
@@ -7,7 +8,10 @@ var mainrouter = require('./routes/main'),
     apirouter = require('./routes/api')
 
 var app = express()
+app.enable('trust proxy');
 app.set("json spaces",2)
+app.use(cors())
+app.use(secure)
 app.use(express.static("public"))
 
 app.use('/', mainrouter)
